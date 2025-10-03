a26cc309c56c48ec3bf689a6c2fc8f38f1c5769e
asaloy <asaloyomonboyeva23@gmail.com>
2025-10-03 10:48:03 +0500

    cleaning up

--- intro.py
+++ intro.py
@@ -1,6 +1,5 @@
 user_name=input("Humoyun")
 user_major=input("Software Enginering")
 user_hobby=input("Ping Pong")
-print("+ user_name +"
-      "+ user_major +""
-      "Hobby:+"+ user_hobby +"")
+print('humoyun')
+
