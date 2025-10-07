5ff59404a02d7e3eabfe5fc7ee4785c51bae207f
HumoyunUlugbekov <ulugbekovhumoyun@gmail.com>
2025-10-07 12:53:38 +0500

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,5 @@
+temperature=float(input('Please enter the temperature in Celcius: '))
+fahrenheit=(temperature*9/5)+32
+kelvin=temperature+273.15
+print(f'Temperature in Fahrenheit: {fahrenheit} fahrenheit')
+print(f'Temperature in Kelvin: {kelvin} K')
