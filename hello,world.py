5f7b516c1b950e5d83217422a592817f5679d7b1
HumoyunUlugbekov <ulugbekovhumoyun@gmail.com>
2025-10-08 21:35:45 +0500

    Add files via upload

---
+++ hello,world.py
@@ -0,0 +1,23 @@
+creature_name=input('Please enter Creature Name: ')
+discovered_planet=input('Please enter Discoverd Planet: ')
+creature_diet=input("Please enter Creature's Diet: ")
+scientist_name=input("Please enter Lead Scientist's Name: ")
+discovery_year=input('Please enter discovery year: ')
+print(f'{'='*50}')
+print('            XENO-BIOLOGY FIELD REPORT')
+print(f'{'='*50}')
+print(f'            ')
+print(f'Scientist:   Dr. {scientist_name}')
+print(f'Discovery Year: {discovery_year}')
+print('              ')
+print(f'{'-'*50}')
+print(f'Creature Name:  {creature_name}')
+print(f'Home Planet:    {discovered_planet}')
+print(f'Primary Diet:   {creature_diet}')
+print('            ')
+print(f'Scientific Designation: {discovered_planet}-{creature_name}-{discovery_year}')
+print(f'{'='*50}')
+
+
+
+
